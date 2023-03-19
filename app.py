1b7a596ca6c56132da19ed2ad66ae69dd3841fce
Suraj Nag <suraj11nag@gmail.com>
2023-03-19 22:39:45 +0530

    new changes

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def recommend():
     return render_template('recommend.html', data=data)
 
 if __name__=='__main__':
-    app.run(debug=True, threaded=False)
+    app.run(debug=True, threaded=False, use_reloader=False)
