a99d717b506d5ddb73b1331586262c5f7aabf33b
Suraj Nag <suraj11nag@gmail.com>
2023-03-20 00:22:58 +0530

    streamlit files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+import streamlit as st
 import pickle
 import numpy as np
 
@@ -7,39 +7,39 @@ pivot_table = pickle.load(open('pivot_table.pkl','rb'))
 books = pickle.load(open('books.pkl','rb'))
 similarity_score = pickle.load(open('similarity_score.pkl','rb'))
 
-app = Flask(__name__)
-
-@app.route('/')
 def index():
-    return render_template('index.html',
-                           book_name=list(popular_df['Book-Title'].values),
-                           author=[f"Author: {author}" for author in popular_df['Book-Author'].values],
-                           image=list(popular_df['Image-URL-M'].values),
-                           votes=[f"Votes: {vote}" for vote in popular_df['num_rating'].values],
-                           ratings=[f"Rating: {rating:.1f}" for rating in popular_df['avg_rating'].values],
-                           )
-@app.route('/recommend')
+    st.markdown("<h1 style='text-align: center;'>Book Recommender</h1>", unsafe_allow_html=True)
+    st.markdown("---")
+    st.markdown("<h2>Popular Books</h2>", unsafe_allow_html=True)
+    for title, author, image, votes, rating in zip(popular_df['Book-Title'], popular_df['Book-Author'], popular_df['Image-URL-M'], popular_df['num_rating'], popular_df['avg_rating']):
+        st.write(f"**{title}** by {author}")
+        st.image(image, width=150)
+        st.write(f"Votes: {votes}, Rating: {rating:.1f}")
+        st.markdown("---")
+
 def recommend_ui():
-    return render_template('recommend.html')
+    st.markdown("<h2>Get Book Recommendations</h2>", unsafe_allow_html=True)
+    user_input = st.text_input("Enter the name of a book you like:")
+    if st.button("Recommend"):
+        if user_input not in pivot_table.index:
+            st.warning("Name not found. Please try again.")
+        else:
+            index_new = np.where(pivot_table.index==user_input)[0][0]
+            similar_book_recommend = sorted(list(enumerate(similarity_score[index_new])),key=lambda x:x[1],reverse=True)[1:6]
+            for items in similar_book_recommend:
+                temp_df = books[books['Book-Title'] == pivot_table.index[items[0]]].drop_duplicates('Book-Title')
+                title, author, image = temp_df['Book-Title'].values[0], temp_df['Book-Author'].values[0], temp_df['Image-URL-M'].values[0]
+                st.write(f"**{title}** by {author}")
+                st.image(image, width=150)
+                st.markdown("---")
 
-@app.route('/recommend_books', methods=['post'])
-def recommend():
-    user_input = request.form.get('user_input')
-    if user_input not in pivot_table.index:
-        return render_template('recommend.html', message='Name not found. Please try again.')
-    index_new = np.where(pivot_table.index==user_input)[0][0]
-    similar_book_recommend = sorted(list(enumerate(similarity_score[index_new])),key=lambda x:x[1],reverse=True)[1:6]
-    data = []
-    for items in similar_book_recommend:
-        book =[]
-        temp_df = books[books['Book-Title'] == pivot_table.index[items[0]]]
-        book.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-        book.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        book.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
-        
-        data.append(book)
-    print(data)
-    return render_template('recommend.html', data=data)
+def main():
+    menu = ["Home", "Recommend"]
+    choice = st.sidebar.selectbox("Select an option", menu)
+    if choice == "Home":
+        index()
+    elif choice == "Recommend":
+        recommend_ui()
 
-if __name__ == '__main__':
-    app.run(debug=True, threaded=False)
+if __name__ == "__main__":
+    main()
